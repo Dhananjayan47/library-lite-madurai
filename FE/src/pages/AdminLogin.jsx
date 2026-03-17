75339dd36917b500ab2cc833c859af2de0d3edd3
Dhananjayan47 <kdhanush181818@gmail.com>
2026-03-17 11:03:07 +0530

    update admin login pages

--- FE/src/pages/AdminLogin.jsx
+++ FE/src/pages/AdminLogin.jsx
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
-import { useState } from "react";
+
+import { useEffect, useState } from "react";
 import { useNavigate,useLocation } from "react-router-dom";
 
 const AdminLogin = () => {
@@ -9,53 +9,42 @@ const AdminLogin = () => {
     const [errNotify, setErrNotify] = useState({ show:false,msg:'' });
     const location = useLocation();
     const exitMsg = location.state;
+
+    useEffect(() => {
+      const notify = () => {
+        // your logic
+      };
+    
+      notify();
+    }, [exitMsg]);
+
+
+    
     const dummyId={name:'vijay', password:'2026'}
+    const handleValidation= (e)=>{
+     e.preventDefault()
+     setDisableBtn(true);
+     if(fieldInputs.name === dummyId.name && fieldInputs.password ===dummyId.password){
+         navigate('/admin-page');
+         return;
+     }
+     setErrNotify( {show:true,msg:'Wrong identity'} )
+     setTimeout(() => {
+         setErrNotify({ show:false,msg:'' });
+         setDisableBtn(false)
+     }, 1500);
+    }
+
+    
    const handleInput =(e)=>{
     const {name,value}=e.target;
-=======
+    setFieldInputs((prev)=>({...prev,[name]:value}));
+  }
 
-import { useNavigate } from "react-router-dom";
-
-const AdminLogin = () => {
-  const navigate = useNavigate();
->>>>>>> ca208d97dce73e66a1029c804853498745d059c0
 
   return (
-    <section className="min-vh-100 d-flex align-items-center justify-content-center">
-      <div className="container">
-        <div className="row justify-content-center">
-          <div className="bg-secondary col-12 col-md-6 col-lg-4 p-4 text-light border rounded shadow-sm">
-            <h3 className="mb-4">Admin Login</h3>
-            <form>
-              <div className="mb-4">
-                <label htmlFor="userName" className="form-label">
-                  Username
-                </label>
-                <input
-                  type="text"
-                  name="name"
-                  id="userName"
-                  className="form-control border border-2 "
-                  placeholder="Enter admin username"
-                />
-
-<<<<<<< HEAD
-   const handleValidation= (e)=>{
-    e.preventDefault()
-    setDisableBtn(true);
-    if(fieldInputs.name === dummyId.name && fieldInputs.password ===dummyId.password){
-        navigate('/admin-page');
-        return;
-    }
-    setErrNotify( {show:true,msg:'Wrong identity'} )
-    setTimeout(() => {
-        setErrNotify({ show:false,msg:'' });
-        setDisableBtn(false)
-    }, 1500);
-   }
    
-   
-    return (
+  
       <section className="flex-fill mx-4 mx-sm-1 my-5">
         <div className="container ">
           <div className="row justify-content-center">
@@ -98,27 +87,10 @@ const AdminLogin = () => {
                 <p >{ errNotify.show ? errNotify.msg :'Admin only page'}</p>
               </form>
             </div>
-=======
-                <label htmlFor="userPassword" className="form-label">
-                  Password
-                </label>
-                <input
-                  type="text"
-                  name="password"
-                  id="userPassword"
-                  className="form-control border border-2 "
-                  placeholder="Enter admin password"
-                />
-              </div>
-              <button
-                onClick={() => navigate("/admin-page")}
-                className=" mb-3 btn btn-primary w-100"
-              >
-                Login
-              </button>
-            </form>
->>>>>>> ca208d97dce73e66a1029c804853498745d059c0
-          </div>
+
+                
+              
+          
         </div>
       </div>
     </section>
