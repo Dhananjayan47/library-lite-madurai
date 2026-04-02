dce6bba7df53013ece27f9a76f5c3ed760c936a6
Dhananjayan47 <kdhanush181818@gmail.com>
2026-04-02 06:00:53 +0530

    added features

--- BE/src/config/db.ts
+++ BE/src/config/db.ts
@@ -4,6 +4,7 @@ import dotenv from "dotenv"
 dotenv.config()
 export const pool = new Pool({
     connectionString:process.env.DB_URL,
+    ssl:{ rejectUnauthorized:false},
     max: 10,
 idleTimeoutMillis: 30000
 });
